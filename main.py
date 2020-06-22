68251e8225005bfe267fef87003b38c214a7544c
Clément HERNANDEZ <clement.hernandez@limayrac.fr>
2020-06-22 13:06:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+tempDeg = "24°"
+ventVit = "12km/h"
+humiTaux = "45%"
+
+meteo = 'aujourd’hui, il fait %s , la vitesse du vent est %s ,l’humidité est %s' % (tempDeg, ventVit, humiTaux)
+print(meteo)
